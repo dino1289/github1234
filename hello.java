b1ae250c6abf174169b9f49638935f1b754b735b
dino1289 <theknot1997@gmail.com>
2021-11-11 19:05:40 +0900

    github is so cool

--- hello.java
+++ hello.java
@@ -7,5 +7,9 @@ System.out.println("update13");
 System.out.println("update13"); //update
 System.out.println("update13"); //update
 //local update eng
+//thru eclipse  update local file and save 
+//which will auto uploaded and ask for commit in
+//source tree and i have to push to github.
+System.out.println("github is so cool");
    }
 }
